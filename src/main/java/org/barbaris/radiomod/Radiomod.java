bc785cbe8c4080313331f22fdddce85fcefd3c50
= <gleb.n1kit1n@yandex.ru>
2023-10-30 19:59:57 +0300

    essential commit

---
+++ src/main/java/org/barbaris/radiomod/Radiomod.java
@@ -0,0 +1,13 @@
+package org.barbaris.radiomod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Radiomod implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/org/barbaris/radiomod/client/RadiomodClient.java
@@ -0,0 +1,13 @@
+package org.barbaris.radiomod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RadiomodClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
